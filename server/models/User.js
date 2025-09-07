85b9fd5c3d8532fbf7360399227272097c3ec2cc
laxman-goud <codewithlaxman92@gmail.com>
2025-09-07 07:57:33 +0530

    feat(server): add user model

---
+++ server/models/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    verifyOtp: { type: String, default: '' },
+    verifyOtpExpiresAt: { type: Number, default: 0 },
+    isVerified: { type: Boolean, default: false },
+    resetOtp: { type: String, default: '' },
+    resetOtpExpiresAt: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+});
+
+const userModel = mongoose.models.user || mongoose.model('user', userSchema);
+
+export default userModel;
