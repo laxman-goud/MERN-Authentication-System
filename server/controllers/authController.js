5a91d286728d3459bd23ad9a526d430b2625780c
laxman-goud <codewithlaxman92@gmail.com>
2025-09-07 20:49:30 +0530

    fix: import userModel to authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import userModel from '../models/User.js';
 
 export const register = async (req, res) => {
     const {name, email, password} = req.body;
