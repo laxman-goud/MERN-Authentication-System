3ec5aeabbabd87af59d443febfd918b6812abe0a
laxman-goud <codewithlaxman92@gmail.com>
2025-09-07 20:26:41 +0530

    feat(server): add auth routes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
+
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +18,8 @@ app.use(cors({ credentials: true }));
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
