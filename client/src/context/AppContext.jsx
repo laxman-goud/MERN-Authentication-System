12aea8d031c63932c5b761e014a5c06193328b0d
laxman-goud <codewithlaxman92@gmail.com>
2025-09-12 15:15:18 +0530

    feat(server): add login and register functionality

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [isLoggedin, setIsLoggedin] = useState(false);
+    const [userData, setUserData] = useState(false);
+
+    const getUserData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/user/data', { withCredentials: true });
+            data.success ? setUserData(data.userData) : toast.error(data.message);
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    const value = {
+        backendUrl,
+        isLoggedin, setIsLoggedin,
+        userData, setUserData,
+        getUserData
+    };
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
