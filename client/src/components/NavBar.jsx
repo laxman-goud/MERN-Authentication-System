bee7a802aa3e4b64e5d0f7fdde738ab007aec633
laxman-goud <codewithlaxman92@gmail.com>
2025-09-13 09:04:21 +0530

    feat(client): add email verify functionality

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -23,6 +23,22 @@ const NavBar = () => {
         }
     }
 
+    const sendVerificationOtp = async () => {
+        try {
+            axios.defaults.withCredentials = true;
+            const {data} = await axios.post(backendUrl + '/api/auth/send-verify-otp');
+            if(data.success){
+                navigate('/verify-email');
+                toast.success(data.message);
+            }
+            else{
+                toast.error(data.message);  
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     return (
         <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
             <img src={assets.logo} alt="logo" />
@@ -33,7 +49,7 @@ const NavBar = () => {
                     <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
                         <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
                             {!userData.isVerified && (
-                                <li className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify email</li>
+                                <li onClick={()=>{sendVerificationOtp()}} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify email</li>
                             )}
                             <li onClick={()=>{logout()}} className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
                         </ul>
