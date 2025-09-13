cb3fbf1b17c2e6b703535f6a46b47bfa63627860
laxman-goud <codewithlaxman92@gmail.com>
2025-09-13 11:20:48 +0530

    chore(server): add client origin to cors

--- server/server.js
+++ server/server.js
@@ -10,12 +10,26 @@ import userRouter from './routes/userRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 connectDB();
-const allowedOrigins = ['http://localhost:5173'];
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://mern-authentication-system-psi.vercel.app"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+}));
+
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
-
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
