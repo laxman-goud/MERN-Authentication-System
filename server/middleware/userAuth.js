06c07200ceb10ae1f57b88e3c974a578cf03b90b
laxman-goud <codewithlaxman92@gmail.com>
2025-09-14 21:29:36 +0530

    docs(server): docs: add comments for userAuth middleware

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,11 +1,17 @@
+// =======================
+// middleware/userAuth.js
+// =======================
+
 import jwt from 'jsonwebtoken';
 
+// Middleware to check JWT from cookies and authenticate user
 const userAuth = (req, res, next) => {
-    const { token } = req.cookies;
+    const { token } = req.cookies; // get token from cookies
     if (!token) {
         return res.status(401).json({ error: 'Unauthorized: Login Again', success: false });
     }
     try {
+        // verify token and attach userId to request
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.id;
         next();
